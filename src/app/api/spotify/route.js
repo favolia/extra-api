e447f9ca892dd7d662e1d85602b71caf43cf4afc
favolia <daisszo1455@gmail.com>
2023-07-27 01:44:15 +0800

    add download url and cover url in spotify

--- src/app/api/spotify/route.js
+++ src/app/api/spotify/route.js
@@ -24,7 +24,21 @@ export async function GET(request) {
             return []
         }
     }
-
+    
+    async function getDownloadLink(id) {
+        try {
+            const { data } = await axios.get(`https://api.spotifydown.com/download/${id}`, {
+                headers: {
+                    'Origin': 'https://spotifydown.com',
+                    'Referer': 'https://spotifydown.com/',
+                  }
+            })
+            return data
+        } catch (error) {
+            console.log(error);
+            return {link: null, metadata: {cover: null}}
+        }
+    }
 
     if (query) {
         try {
@@ -42,17 +56,65 @@ export async function GET(request) {
         try {
             const { data } = await axios.get(`${endpoint}/info/track?url=${url}`)
             const lyrics = await getLyrics(data.id)
+            const meta = await getDownloadLink(data.id)
             if (data < 1) return NextResponse.json({ status: false, message: 'Not found' }, { status: 404, statusText: 'Track not found' }) 
             return NextResponse.json({
                 status: true,
                 message: 'success',
+                cover: meta.metadata.cover,
+                download: meta.link,
                 data,
-                lyrics
+                lyrics,
             }, { status: 200})
         } catch (error) {
             return NextResponse.json({ status: false, message: error.message}, { status: 500, statusText: error.message })
         }
     }
-
-
   }
+
+
+// QUERY
+//          /api/query?={song name}                         
+//          RESPONSE ✔ 200:                          Response ❌ 404:
+//          ----------------------------------------------------------
+//          status: Boolean                          status: Boolean
+//          message: String                          message: String
+//          data: <Array>: [<Object>: {
+//              title: String 
+//              artist: String 
+//              album: String
+//              url: Url
+//              id: String
+//              release_date: String
+//              duration: String
+//              durationMs: Number
+//              thumbnail: Url
+//              preview_mp3: Url
+//          }]
+
+
+// URL
+//          /api/url?={track URL}                         
+//          RESPONSE ✔ 200:                          Response ❌ 404:                 Response ❌ 500:
+//          --------------------------------------------------------------------------------------------
+//          status: Boolean                          status: Boolean                   status: Boolean
+//          message: String                          message: String                   mesage: String
+//          cover: Url
+//          download: Url
+//          data: <Array>: [<Object>: {
+//              title: String
+//              artist: String
+//              album: String
+//              url: Url
+//              id: String
+//              release_date: String
+//              duration: String
+//              durationMs: Number
+//              thumbnail: Url
+//              preview_mp3: Url
+//          }]
+//          lyrics: <Array> [<Object> {
+//              timeTag: String
+//              words: String
+//          }]
+
