4b37d225f22d3df3b3eb8bf8bdffcb4d37181315
favolia <daisszo1455@gmail.com>
2024-02-26 20:46:20 +0800

    feat: update youtube api

--- src/app/api/youtube/functions.js
+++ src/app/api/youtube/functions.js
@@ -19,21 +19,33 @@ export async function youtubedl(url) {
 
         return {
             status: true,
-            data: [
-              {
-                  resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').text().trim() || null,
-                  download: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').attr('href') || null,
-              },
-              {
-                  resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').text().trim() || null,
-                  download: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').attr('href') || null
-              }
-          ]
+            message: 'success',
+            data: {
+                title: $('div.border-bg > div.video-title').text().trim() || '',
+                channel_profile: $('div.channelDetails.col-sm-8 > a > img.channelThumb').attr('src'),
+                channel_name: $('div.channelDetails.col-sm-8 > a.uploader-name').text() || '',
+                channel_subscription: $('div.channelDetails.col-sm-8 > div > span.subscripberNum').text(),
+                views: $('div.text-right.col-sm-4 > div:nth-child(1)').text().trim(),
+                likes: $('div.text-right.col-sm-4 > div.font-size-13 > span#likeButton').text().trim() || '0',
+                release: $('div.tab-pane.fade.active.in > h4.font-weight-bold').text().trim().replace('Video Link', ''), 
+                description: $('div.tab-pane.fade.active.in > div.overflow-hidden').text().trim() || '', 
+                videos: [
+                    {
+                        resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').text().trim() || '',
+                        download: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').attr('href') || null,
+                    },
+                    {
+                        resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').text().trim() || '',
+                        download: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').attr('href') || null
+                    }
+                ]
+            }
         }
     } catch (error) {
         return {
             status: false,
-            message: error.message || 'Something went wrong'
+            message: error.message || 'Something went wrong',
+            data: null
         }
     }
 }
