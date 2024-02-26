7ccaf90d164815d7cc6833f6376e4ce42dcc8ccc
favolia <daisszo1455@gmail.com>
2024-02-26 21:08:57 +0800

    fix: download url

--- src/app/api/youtube/functions.js
+++ src/app/api/youtube/functions.js
@@ -4,6 +4,33 @@ const axios = require('axios');
 
 const endpoint = 'https://clipzag.com/watch?v='
 
+const getHeader = async ({ url }) => {
+    const countsize = await axios.head(url);
+    const size = await countsize.headers['content-length'];
+    return size;
+};
+
+const originalUrl = async (u) => await fetch(u, {
+    headers: {
+        accept: "*/*",
+        "user-agent": "PostmanRuntime/7.32.2",
+        "content-type": "application/x-www-form-urlencoded"
+    },
+}).then(res => res.url).catch(err => u);
+
+const sizeToMB = (size, unit) => {
+    const sizeInBytes = {
+        'B': size,
+        'KB': size * 1024,
+        'MB': size * 1024 * 1024,
+        'GB': size * 1024 * 1024 * 1024,
+        'TB': size * 1024 * 1024 * 1024 * 1024,
+        'PB': size * 1024 * 1024 * 1024 * 1024 * 1024,
+    };
+
+    return sizeInBytes[unit] / (1024 * 1024);
+};
+
 export async function youtubedl(url) {
     try {
         var id = await getYouTubeID(url);
@@ -17,6 +44,17 @@ export async function youtubedl(url) {
         const { data } = await axios.get(`${endpoint}${id}`)
         const $ = cheerio.load(data)
 
+        const downloadsDetail = [
+            {
+                resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').text().trim() || '',
+                download: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').attr('href') || null,
+            },
+            {
+                resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').text().trim() || '',
+                download: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').attr('href') || null
+            }
+        ]
+
         return {
             status: true,
             message: 'success',
@@ -27,18 +65,18 @@ export async function youtubedl(url) {
                 channel_subscription: $('div.channelDetails.col-sm-8 > div > span.subscripberNum').text(),
                 views: $('div.text-right.col-sm-4 > div:nth-child(1)').text().trim(),
                 likes: $('div.text-right.col-sm-4 > div.font-size-13 > span#likeButton').text().trim() || '0',
-                release: $('div.tab-pane.fade.active.in > h4.font-weight-bold').text().trim().replace('Video Link', ''), 
-                description: $('div.tab-pane.fade.active.in > div.overflow-hidden').text().trim() || '', 
-                videos: [
-                    {
-                        resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').text().trim() || '',
-                        download: $('div.downloadbox > div.btn-group.btn-group-justified > a:first').attr('href') || null,
-                    },
-                    {
-                        resolution: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').text().trim() || '',
-                        download: $('div.downloadbox > div.btn-group.btn-group-justified > a:last').attr('href') || null
-                    }
-                ]
+                release: $('div.tab-pane.fade.active.in > h4.font-weight-bold').text().trim().replace('Video Link', ''),
+                description: $('div.tab-pane.fade.active.in > div.overflow-hidden').text().trim() || '',
+                videos: await Promise.all(downloadsDetail.map(async (item) => {
+                    const oriUrl = await originalUrl(item.download);
+                    const sizeInBytes = await getHeader({ url: oriUrl });
+                    const sizeInMB = sizeToMB(sizeInBytes, 'B');
+                    return {
+                        resolution: item.resolution,
+                        download: oriUrl,
+                        size: sizeInMB,
+                    };
+                }))
             }
         }
     } catch (error) {
